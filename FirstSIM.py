9f7597666a2df2e4405a8771cc5803d76a2cf61d
ethanrp2 <ethanrp2@illinois.edu>
2022-10-31 02:28:56 -0500

    added state space matricies

--- FirstSIM.py
+++ FirstSIM.py
@@ -1,14 +1,36 @@
 # This class creates a basic simulation of a rocket through its 5 stages with a constant time step then graphs it
 
 #Define Variables
-time_step = 10e-3
+from time import time
+
+
+dt = 10e-3
 current_time = 0
 
 motor_thrust = 90 #  (N)
 rocket_mass = 0.76 # (Kg)
 rocket_weight = rocket_mass * 9.8 # (N)
 
+altitude = 0
+velocity = 0
+acceleration = 0
 
+dict_counter = 0
+
+# State Space Matricies
+# --> x = Ax + Bu
+x = [[altitude],
+     [velocity],
+     [acceleration]]
+
+A = [[1, dt, 0.5*(dt**2)],
+     [0, 1, dt],
+     [0, 0, 0]
+]
+
+
+
+# Simulation State Dictionary
 sim_dict = { 
     "altitude": [],
     "velocity": [],
@@ -16,10 +38,12 @@ sim_dict = {
     "time": []
 }
 
-def updateState(): {
-    
-}
 
+def updateState(): 
+    
+    altitude = sim_dict["altitude"][dict_counter] + sim_dict["velocity"][dict_counter]*dt + 0.5*sim_dict["acceleration"][dict_counter]*dt**2
+
+    
 def launch():
     return 1
 
@@ -42,8 +66,7 @@ def recovery():
 def ground_hit():
     return 1
 
-boost()
-
+# boost()
 
 
 
