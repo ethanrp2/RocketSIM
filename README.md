b255e104cb8247477885e418b9be34b1d3b1969f
ethanrp2 <ethanrp2@illinois.edu>
2022-12-18 11:07:20 -0500

    added LQR Controller

--- README.md
+++ README.md
@@ -3,15 +3,14 @@
 - Plots Altitude, Velocity, Acceleration
 - Accounts for Aerodynamic Drag
 - Constantly calculates and predicts apogee based on current flap configuration
-- Active Drag Control System (ADCS) with PID controller for rocket to reach desired apogee (5200 m)
+- Active Drag Control System (ADCS) with PID/LQR controller for rocket to reach desired apogee (5200 m)
 - Simulated Noisy Sensor Data from IMU/Barometer
 - State Estimation using Kalman Filter with inputted noisy sensor data
 
  
 ## Future Additions to RocketSIM
- - LQR Controller for ADCS
  - Randomized wind vector
- - Implement RK4 step
+ - RK4 propogation
 
 ## Rocket Simulation Plot
 ![hello](assets/sim_plot_screenshot.png)
@@ -22,3 +21,5 @@
  - Air density equation relative to altitude --> (https://www.homerenergy.com/products/pro/docs/latest/altitude.html)
  - MS5611 Barometer Datasheet --> (https://www.te.com/commerce/DocumentDelivery/DDEController?Action=showdoc&DocId=Data+Sheet%7FMS5611-01BA03%7FB3%7Fpdf%7FEnglish%7FENG_DS_MS5611-01BA03_B3.pdf%7FCAT-BLPS0036)
  - LSM9DS1 Intertial Measurement Unit (IMU) Datasheet --> (https://www.st.com/resource/en/datasheet/lsm9ds1.pdf)
+ - LQR Control Example --> (https://automaticaddison.com/linear-quadratic-regulator-lqr-with-python-code-example/#LQR_Python_Code)
+ - State Space Model: Derivation of A and B matricies --> (https://automaticaddison.com/how-to-derive-the-state-space-model-for-a-mobile-robot/)
--- src/controls.py
+++ src/controls.py
@@ -2,16 +2,29 @@
 ## Examples of control include aerodynamic drag systems, roll control, etc.
 import src.atmosphere as atmosphere
 import src.constants as constants
+import numpy as np
+import constants as const
+# File Variables:
+dt = const.dt
 
-#Predicted State Variables:
-alt_pred = 0
-vel_pred = 0
-acc_pred = 0
-
-#PID Variables:
+# PID Variables:
 Kp = 0.0035
 Ki = 0.000001
 Kd = 0.0000001
+
+# LQR Variables/Matricies
+A = np.array([1.0, dt, 0.5*(dt**2)],
+             [0, 1.0, dt],
+             [0, 0, 1.0])
+
+B = np.array([0.5*(dt**2)],
+             [dt],
+             [1.0])
+
+Q = np.array([1.0, 0, 0],
+             [0, 1.0, 0],
+             [0, 0, 1.0])
+R = np.array([1.0])
 
 error_sum = 0
 error_derivative = 0
@@ -22,6 +35,31 @@ def active_aero_drag(x_state, flap_ext):
     vel = x_state[1]
     A_flaps = flap_ext * constants.flap_width * constants.flap_length_max
     return 0.5*atmosphere.density(z)*(vel**2)*constants.C_d_flaps*(A_flaps)
+
+def active_drag_LQR(predicted_apogee):
+
+    x_error = predicted_apogee - constants.DESIRED_APOGEE
+
+    N = 50
+    P = [None] * (N + 1)
+    Qf = Q
+    P[N] = Qf
+ 
+    for i in range(N, 0, -1):
+        P[i-1] = Q + A.T @ P[i] @ A - (A.T @ P[i] @ B) @ np.linalg.pinv(
+            R + B.T @ P[i] @ B) @ (B.T @ P[i] @ A)      
+ 
+    K = [None] * N
+    u = [None] * N
+ 
+    for i in range(N): 
+        K[i] = -np.linalg.pinv(R + B.T @ P[i+1] @ B) @ B.T @ P[i+1] @ A
+        u[i] = K[i] @ x_error
+ 
+    u_star = u[N-1]
+ 
+    return u_star
+ 
 
 def active_drag_PID(predicted_apogee):
     global error_sum
